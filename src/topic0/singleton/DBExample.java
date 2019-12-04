d0981c2f8a0b16f48327c2f0769c22fb74d8db45
felipeperezg <53921531+felipeperezg@users.noreply.github.com>
2019-12-04 12:26:25 -0300

    Addition of DB Singleton Example

---
+++ src/topic0/singleton/DBExample.java
@@ -0,0 +1,26 @@
+package topic0.singleton;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBExample {
+    private static DBExample instance= new DBExample();
+    private static Connection conn=null;
+    //Constructor is setted to private to avoid further instances
+    private DBExample(){ }
+
+    public static DBExample getInstance(){
+        return instance;
+    }
+    public static Connection getDBConnection(){
+        try{
+            if (conn==null){
+                Class.forName("com.mysql.jdbc.Driver");
+                conn=DriverManager.getConnection("jdbc:mysql://localhost/xx","root","pass123");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+}
---
+++ src/topic0/singleton/DBExampleTest.java
@@ -0,0 +1,22 @@
+package topic0.singleton;
+
+import java.sql.*;
+
+public class DBExampleTest {
+    public static void main(String[] args) {
+        Connection conn=DBExample.getDBConnection();
+        try{
+            String query = "Select * from users";
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1)+" "+resultSet.getString(2));
+
+            }
+
+        }catch (Exception e){
+            System.out.println("Error during connection");
+        }
+    }
+}
